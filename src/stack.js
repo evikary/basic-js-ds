338e7cb97a6cb106e53e3ec86694958e0519ca60
evikary <daria_more10@mail.ru>
2023-12-21 22:14:56 +0300

    feat: add stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  arr = [];
+
+  push(element) {
+    this.arr.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr[this.arr.length - 1];
   }
 }
 
+// const stack = new Stack();
+
+// console.log(stack);
+// stack.push(1);
+// stack.push(666);
+// console.log(stack.pop());
+// console.log(stack);
+// console.log(stack.peek());
+
 module.exports = {
-  Stack
+  Stack,
 };
